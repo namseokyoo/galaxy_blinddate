efc223003fc73e57d1494e1733fd8dd037eba209
namseokyoo <namseok.yoo@gmail.com>
2024-10-18 22:08:02 +0900

    deploy

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-import os
-import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import streamlit as st
 
 # Streamlit Secrets에서 자격 증명 정보 가져오기
 credentials_info = dict(st.secrets["google_credentials"])  # 딕셔너리로 변환
-
-# 임시 파일에 자격 증명 정보 저장
-with open("temp_credentials.json", "w") as f:
-    json.dump(credentials_info, f)
 
 # Google Spreadsheet와 연결하는 함수
 
@@ -20,8 +14,8 @@ def get_google_sheet_row_count(sheet_url, sheet_name):
         print("Loading credentials...")
         scope = ["https://spreadsheets.google.com/feeds",
                  "https://www.googleapis.com/auth/drive"]
-        creds = ServiceAccountCredentials.from_json_keyfile_name(
-            "temp_credentials.json", scope)
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(
+            credentials_info, scope)
         client = gspread.authorize(creds)
         print("Credentials loaded successfully.")
 
@@ -46,8 +40,8 @@ st.markdown("<h1 style='text-align: center;'>은하수 소개팅 회원 현황</
             unsafe_allow_html=True)
 
 # 사용자로부터 두 개의 스프레드시트 URL 입력 받기
-male_sheet_url = "https://docs.google.com/spreadsheets/d/10qhSG6BNsxb89JiNHo1OL95YUS8MjB6PICzVB_wXaso/edit?gid=1277522226#gid=1277522226"
-female_sheet_url = "https://docs.google.com/spreadsheets/d/1hkEnvCfXePw3Ng2QO6Pd-mOJaxha6CBGGvS32TVs03E/edit?gid=716709069#gid=716709069"
+male_sheet_url = st.secrets["sheet_urls"]["male"]
+female_sheet_url = st.secrets["sheet_urls"]["female"]
 
 # 남성 회원수 가져오기
 male_sheet_name = "남성"
@@ -71,12 +65,6 @@ if male_sheet_url and female_sheet_url:
             st.markdown(f"<div style='text-align: center; font-size: 24px;'><strong>여성 회원 수</strong><br><span style='font-size: 36px;'>{
                         female_row_count}</span></div>", unsafe_allow_html=True)
 
-        # # 추가적인 스타일링
-        # st.markdown("<hr style='border:1px solid #eee;'>", unsafe_allow_html=True)
-        # st.markdown("<h3 style='text-align: center;'>회원 수 통계</h3>", unsafe_allow_html=True)
-        # st.markdown(f"<div style='text-align: center;'><strong>남성 회원 수</strong>: {male_row_count}명</div>", unsafe_allow_html=True)
-        # st.markdown(f"<div style='text-align: center;'><strong>여 회원 수</strong>: {female_row_count}명</div>", unsafe_allow_html=True)
-
     except gspread.exceptions.SpreadsheetNotFound:
         st.error("Spreadsheet not found. Please check the URL.")
     except gspread.exceptions.WorksheetNotFound:
@@ -84,6 +72,3 @@ if male_sheet_url and female_sheet_url:
     except Exception as e:
         st.error(f"An unexpected error occurred: {e}")
         st.error(f"Error details: {str(e)}")  # 예외 메시지 출력
-
-# 임시 파일 삭제
-os.remove("temp_credentials.json")
